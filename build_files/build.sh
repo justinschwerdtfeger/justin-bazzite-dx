27aa33288f47cfd6ad2be652558ccf3ea1f917ed
Justin <justinschwerdtfeger245@gmail.com>
2026-02-17 15:26:28 -0600

    Add "install" to install pipx

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # Install pipx
-dnf5 -y pipx
+dnf5 -y install pipx
 
 # Install Vicinae from Copr
 dnf5 -y copr enable quadratech188/vicinae
