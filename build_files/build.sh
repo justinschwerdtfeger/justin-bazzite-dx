9ce1af73b922f1e7be8d66dd69a1d9633033fdee
Justin <justinschwerdtfeger245@gmail.com>
2026-02-17 14:48:03 -0600

    Install Vicinae

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,6 +9,11 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
+# Install Vicinae from Copr
+dnf5 -y copr enable quadratech188/vicinae
+dnf5 -y install vicinae
+dnf5 -y copr disable quadratech188/vicinae
+
 # Install Niri from Copr
 dnf5 -y copr enable yalter/niri
 dnf5 -y install niri
